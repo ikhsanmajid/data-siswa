c838b78bed11a5b804463b7b8fc5dd3de82ff665
Syaharudin Ikhsan Majid <ikhsanmajid@gmail.com>
2018-07-06 15:08:02 +0700

    Update koneksi.php

--- koneksi/koneksi.php
+++ koneksi/koneksi.php
@@ -1,7 +1,8 @@
 <?php
-defined('BASEPATH') or die("FILE PROTECTED !! <br> DILARANG DIRECT ACCESS");
+//defined('BASEPATH') or die("FILE PROTECTED !! <br> DILARANG DIRECT ACCESS");
 try {
 	$koneksi = new PDO("mysql:host=db4free.net;dbname=ikhsanmajid","sandaljebat7","7jebatsendal");
+	echo "SUKSES";
 	$koneksi->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 }catch (PDOException $gagal){
 	echo $gagal->getMessage();
